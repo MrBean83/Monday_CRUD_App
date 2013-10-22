a80a6eecf7c795523fb1f10cdc5b2b6532583a81
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-21 19:03:55 -0500

    FINAL Monday CRUD App

--- crud_app/public/js/application.js
+++ crud_app/public/js/application.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  
+  // Click on create a new person
+  $("#new_person_link").on("click", function(event){
+    event.preventDefault();
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $.get('/people/new', function(response) {
+      $("#new_person_link").hide();
+      $(".container").append(response);
+    });
+  });
+
+  // Submit the form for a new person
+  $('.container').on('submit', 'form', function(event) {
+    event.preventDefault();
+    var data = $('form').serialize();
+    $.post('/people', data, function(response){
+      $('form').remove();
+      $('#new_person_link').before(response);
+      $("#new_person_link").show();
+    });
+  });
+
+  // Deleting a person from the index
+  $('.container').on('click', '.delete_user_link', function(event) {
+    event.preventDefault();
+    var that = this
+
+    var url = $(that).attr('href')
+
+    $.get(url, function(response){
+      console.log(response);
+      $(that).parent().remove();
+    });
+  });
 });
